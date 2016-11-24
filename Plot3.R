d55f0cc63320bc361923a79a2010cfef8ba6b6df
Shashank Shekhar <sshekhar.special@gmail.com>
2016-11-25 04:13:57 +0530

    Final edit

--- Plot3.R
+++ Plot3.R
@@ -7,7 +7,7 @@ lines(power_sub$Time, power_sub$Sub_metering_2 ,type = 'l' ,ylab = NA ,xlab = NA
 lines(power_sub$Time, power_sub$Sub_metering_3 ,type = 'l' ,ylab = NA ,xlab = NA, col = 'blue' )
 title(ylab = 'Energy Sub metering')
 axis(1, at = seq(1,3,1) ,labels = c("Thu","Fri","Sat"))
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), lty = c(1,1,1), border = 'black')
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), lty = c(1,1,1), border = 'black', cex = 0.5)
 
 # Copy to png file
 dev.copy(png, file = "plot3.png")
--- Plot4.R
+++ Plot4.R
@@ -5,7 +5,7 @@ source('Read_subset.R')
 par(mfrow = c(2,2))
 
 # First plot
-plot(power_sub$Time, power_sub$Global_active_power,type = 'l' ,ylab = 'Global Active Power (kilowatts)' ,xlab = NA )
+plot(power_sub$Time, power_sub$Global_active_power,type = 'l' ,ylab = 'Global Active Power' ,xlab = NA )
 axis(1, at = seq(1,3,1) ,labels = c("Thu","Fri","Sat"))
 
 # Second plot
@@ -18,12 +18,11 @@ lines(power_sub$Time, power_sub$Sub_metering_2 ,type = 'l' ,ylab = NA ,xlab = NA
 lines(power_sub$Time, power_sub$Sub_metering_3 ,type = 'l' ,ylab = NA ,xlab = NA, col = 'blue' )
 title(ylab = 'Energy Sub metering')
 axis(1, at = seq(1,3,1) ,labels = c("Thu","Fri","Sat"))
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), lty = c(1,1,1), border = 'black')
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), lty = c(1,1,1), cex=0.5)
 
 # Fourth plot
 plot(power_sub$Time, power_sub$Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = 'datetime' )
 axis(1, at = seq(1,3,1) ,labels = c("Thu","Fri","Sat"))
-
 
 # Copy to png file
 dev.copy(png, file = "plot4.png")
